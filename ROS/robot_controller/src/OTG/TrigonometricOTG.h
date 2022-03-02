b7fbbae051a8726921df3e15c983a628e7669d32
Zhao <hzhao9@Swarmctrl-09lin.ee.e.uh.edu>
2022-03-02 10:41:19 -0600

    update comments

--- ROS/robot_controller/src/OTG/TrigonometricOTG.h
+++ ROS/robot_controller/src/OTG/TrigonometricOTG.h
@@ -14,14 +14,66 @@ using namespace std;
 
 class TrigonometricOTG {
 public:
+	/**
+	* Constructor
+	* Creates TrigonometricOTG object
+	*/
 	TrigonometricOTG();
+	/**
+	* Constructor
+	* Creates TrigonometricOTG object
+	* @param num_dof number of degree of freedom
+	* @param rate control rate
+	*/
 	TrigonometricOTG(int num_dof,double rate);
+	/**
+	* Constructor
+	* Creates TrigonometricOTG object
+	* @param num_dof number of degree of freedom
+	* @param mJ maximum jerk
+	* @param mA maximum accerleration
+	* @param mV maximum velocity
+	* @param rate control rate
+	*/
 	TrigonometricOTG(int num_dof, double mJ, double mA, double mV, double rate);
+	/**
+	* Constructor
+	* Creates TrigonometricOTG object
+	* @param num_dof number of degree of freedom
+	* @param mJ maximum jerk
+	* @param mA maximum accerleration
+	* @param mV maximum velocity
+	* @param rate control rate
+	* @param a0 initial accelration
+	* @param v0 initial velocity
+	* @param p0 initial position
+	* @pram pG target position
+	* @param alpha time factor ratio
+	*/
 	TrigonometricOTG(int num_dof, vector<double> mJ, vector<double> mA, vector<double> mV, vector<double> a0, vector<double> v0, vector<double> p0, vector<double> pG, vector<double> alpha, double rate);
+	/**
+	* Constructor
+	* Creates TrigonometricOTG object
+	* @param num_dof number of degree of freedom
+	* @param mJ maximum jerk
+	* @param mA maximum accerleration
+	* @param mV maximum velocity
+	* @param rate control rate
+	* @param a0 initial accelration
+	* @param v0 initial velocity
+	* @param p0 initial position
+	* @pram pG target position
+	* @param alpha time factor ratio
+	*/
 	TrigonometricOTG(int num_dof, double mJ, double mA, double mV, vector<double> a0, vector<double> v0, vector<double> p0, vector<double> pG, double alpha, double rate);
-
+	/**
+	* Destructor
+	*/
 	~TrigonometricOTG();
 
+	/**
+	* struct to store the computed time factors of trajectory
+	*/
 	struct Times{
 		bool brake = false;
 		double dist;
@@ -31,40 +83,336 @@ public:
 		vector<double> twoPiece; // t11,t22,t33,t1,t2,ta1,ta2,ta3,tb1,tb2,t4,tc1,tc2,tc3;
 		vector<double> onePiece; // ta1,ta2,ta3,tb1,tb2,t4,tc1,tc2,tc3;
 	};
-
+	/**
+	* compute time factors of type I
+	* @param num_dof number of degree of freedom
+	* @param mJ maximum jerk
+	* @param mA maximum accerleration
+	* @param mV maximum velocity
+	* @param a0 initial accelration
+	* @param v0 initial velocity
+	* @param p0 initial position
+	* @pram pG target position
+	* @param alpha time factor ratio
+	*/
 	vector<vector<double>> typeI(double a0, double v0, double p0, double pG, double alpha, double mJ, double mA, double mV);
+	/**
+	* compute time factors of type II
+	* @param num_dof number of degree of freedom
+	* @param mJ maximum jerk
+	* @param mA maximum accerleration
+	* @param mV maximum velocity
+	* @param a0 initial accelration
+	* @param v0 initial velocity
+	* @param p0 initial position
+	* @pram pG target position
+	* @param alpha time factor ratio
+	*/
 	vector<vector<double>> typeII(double a0, double v0, double p0, double pG, double alpha, double mJ, double mA, double mV);
+	/**
+	* compute time factors of type III
+	* @param num_dof number of degree of freedom
+	* @param mJ maximum jerk
+	* @param mA maximum accerleration
+	* @param mV maximum velocity
+	* @param a0 initial accelration
+	* @param v0 initial velocity
+	* @param p0 initial position
+	* @pram pG target position
+	* @param alpha time factor ratio
+	*/
 	vector<vector<double>> typeIII(double a0, double v0, double p0, double pG, double alpha, double mJ, double mA, double mV);
+	/**
+	* compute time factors of type IV
+	* @param num_dof number of degree of freedom
+	* @param mJ maximum jerk
+	* @param mA maximum accerleration
+	* @param mV maximum velocity
+	* @param a0 initial accelration
+	* @param v0 initial velocity
+	* @param p0 initial position
+	* @pram pG target position
+	* @param alpha time factor ratio
+	*/
 	vector<vector<double>> typeIV(double a0, double v0, double p0, double pG, double alpha, double mJ, double mA, double mV);
+	/**
+	* compute time factors of type V
+	* @param num_dof number of degree of freedom
+	* @param mJ maximum jerk
+	* @param mA maximum accerleration
+	* @param mV maximum velocity
+	* @param a0 initial accelration
+	* @param v0 initial velocity
+	* @param p0 initial position
+	* @pram pG target position
+	* @param alpha time factor ratio
+	*/
 	vector<vector<double>> typeV(double a0, double v0, double p0, double pG, double alpha, double mJ, double mA, double mV);
+	/**
+	* compute time factors of type VI
+	* @param num_dof number of degree of freedom
+	* @param mJ maximum jerk
+	* @param mA maximum accerleration
+	* @param mV maximum velocity
+	* @param a0 initial accelration
+	* @param v0 initial velocity
+	* @param p0 initial position
+	* @pram pG target position
+	* @param alpha time factor ratio
+	*/
 	vector<vector<double>> typeVI(double a0, double v0, double p0, double pG, double alpha, double mJ, double mA, double mV);
+	/**
+	* compute time factors of type VII
+	* @param num_dof number of degree of freedom
+	* @param mJ maximum jerk
+	* @param mA maximum accerleration
+	* @param mV maximum velocity
+	* @param a0 initial accelration
+	* @param v0 initial velocity
+	* @param p0 initial position
+	* @pram pG target position
+	* @param alpha time factor ratio
+	*/
 	vector<vector<double>> typeVII(double a0, double v0, double p0, double pG, double alpha, double mJ, double mA, double mV);
+	/**
+	* compute time factors of type VIII
+	* @param num_dof number of degree of freedom
+	* @param mJ maximum jerk
+	* @param mA maximum accerleration
+	* @param mV maximum velocity
+	* @param a0 initial accelration
+	* @param v0 initial velocity
+	* @param p0 initial position
+	* @pram pG target position
+	* @param alpha time factor ratio
+	*/
 	vector<vector<double>> typeVIII(double a0, double v0, double p0, double pG, double alpha, double mJ, double mA, double mV);
-
+	/**
+	* Math to compute time factors of type I
+	* @param num_dof number of degree of freedom
+	* @param mJ maximum jerk
+	* @param mA maximum accerleration
+	* @param mV maximum velocity
+	* @param a0 initial accelration
+	* @param v0 initial velocity
+	* @param p0 initial position
+	* @pram pG target position
+	* @param alpha time factor ratio
+	*/
 	vector<vector<double>> computeTypeI(double a0, double v0, double p0, double pG, double alpha, double mJ, double mA, double mV, double sa, double sv);
+	/**
+	* Math to compute time factors of type II
+	* @param num_dof number of degree of freedom
+	* @param mJ maximum jerk
+	* @param mA maximum accerleration
+	* @param mV maximum velocity
+	* @param a0 initial accelration
+	* @param v0 initial velocity
+	* @param p0 initial position
+	* @pram pG target position
+	* @param alpha time factor ratio
+	*/
 	vector<vector<double>> computeTypeII(double a0, double v0, double p0, double pG, double alpha, double mJ, double mA, double mV, double sa, double sv);
+	/**
+	* Math to compute time factors of type III
+	* @param num_dof number of degree of freedom
+	* @param mJ maximum jerk
+	* @param mA maximum accerleration
+	* @param mV maximum velocity
+	* @param a0 initial accelration
+	* @param v0 initial velocity
+	* @param p0 initial position
+	* @pram pG target position
+	* @param alpha time factor ratio
+	*/
 	vector<vector<double>> computeTypeIII(double a0, double v0, double p0, double pG, double alpha, double mJ, double mA, double mV, double sa, double sv);
+	/**
+	* Math to compute time factors of type IV
+	* @param num_dof number of degree of freedom
+	* @param mJ maximum jerk
+	* @param mA maximum accerleration
+	* @param mV maximum velocity
+	* @param a0 initial accelration
+	* @param v0 initial velocity
+	* @param p0 initial position
+	* @pram pG target position
+	* @param alpha time factor ratio
+	*/
 	vector<vector<double>> computeTypeIV(double a0, double v0, double p0, double pG, double alpha, double mJ, double mA, double mV, double sa, double sv);
+	/**
+	* Math to compute time factors of type V
+	* @param num_dof number of degree of freedom
+	* @param mJ maximum jerk
+	* @param mA maximum accerleration
+	* @param mV maximum velocity
+	* @param a0 initial accelration
+	* @param v0 initial velocity
+	* @param p0 initial position
+	* @pram pG target position
+	* @param alpha time factor ratio
+	*/
 	vector<vector<double>> computeTypeV(double a0, double v0, double p0, double pG, double alpha, double mJ, double mA, double mV, double sa, double sv);
+	/**
+	* Math to compute time factors of type VI
+	* @param num_dof number of degree of freedom
+	* @param mJ maximum jerk
+	* @param mA maximum accerleration
+	* @param mV maximum velocity
+	* @param a0 initial accelration
+	* @param v0 initial velocity
+	* @param p0 initial position
+	* @pram pG target position
+	* @param alpha time factor ratio
+	*/
 	vector<vector<double>> computeTypeVI(double a0, double v0, double p0, double pG, double alpha, double mJ, double mA, double mV, double sa, double sv);
+	/**
+	* Math to compute time factors of type VII
+	* @param num_dof number of degree of freedom
+	* @param mJ maximum jerk
+	* @param mA maximum accerleration
+	* @param mV maximum velocity
+	* @param a0 initial accelration
+	* @param v0 initial velocity
+	* @param p0 initial position
+	* @pram pG target position
+	* @param alpha time factor ratio
+	*/
 	vector<vector<double>> computeTypeVII(double a0, double v0, double p0, double pG, double alpha, double mJ, double mA, double mV, double sa, double sv);
+	/**
+	* Math to compute time factors of type VIII
+	* @param num_dof number of degree of freedom
+	* @param mJ maximum jerk
+	* @param mA maximum accerleration
+	* @param mV maximum velocity
+	* @param a0 initial accelration
+	* @param v0 initial velocity
+	* @param p0 initial position
+	* @pram pG target position
+	* @param alpha time factor ratio
+	*/
 	vector<vector<double>> computeTypeVIII(double a0, double v0, double p0, double pG, double alpha, double mJ, double mA, double mV, double sa, double sv);
-
+	/**
+	* Check if the results are valid
+	* @param candidate potential time factor solution
+	*/
 	vector<double> isValid(vector<double> candidate);
+	/**
+	* filter all potnetial time factor set and find the minimum time trajectpry time factors which satisfy all conditions
+	* @param candidates all trjaectory time factor sets
+	* @param Jpeak maximum jerk
+	* @param Apeak maximum accerleration
+	* @param Vpeak maximum velocity
+	* @param a0 initial accelration
+	* @param v0 initial velocity
+	* @param p0 initial position
+	* @pram pG target position
+	* @param alpha time factor ratio
+	*/
 	vector<double> filterResults(vector<vector<double>>& candidates, double a0, double v0, double p0, double pG, double alpha, double Jpeak, double Apeak, double Vpeak);
+	/**
+	* Generate trajectory profile
+	* @param candidates all trjaectory time factor sets
+	* @param mJ maximum jerk
+	* @param mA maximum accerleration
+	* @param mV maximum velocity
+	* @param a0 initial accelration
+	* @param v0 initial velocity
+	* @param p0 initial position
+	* @pram pG target position
+	* @param alpha time factor ratio
+	* @param t current time
+	* @param t11 t22 t33 t1 t2 t3 time factors
+	* @param sa direction
+	*/
 	vector<double> profileGeneratorH(double a0, double v0, double p0, double alpha, double mJ, double mA, double mV, double t11, double t22, double t33, double t1, double t2, double sa, double t);
+	/**
+	* main function to compute time factors and profile
+	* @param Jpeak maximum jerk
+	* @param Apeak maximum accerleration
+	* @param Vpeak maximum velocity
+	* @param a0 initial accelration
+	* @param v0 initial velocity
+	* @param p0 initial position
+	* @pram pG target position
+	* @param alpha time factor ratio
+	* @param duration total trajectory time
+	*/
 	vector<vector<double>> trajGeneratorT(double a0, double v0, double p0, double pG, double alpha, double mJ, double mA, double mV, double duration);
+	/**
+	* generate trajectory profile in once
+	*/
 	vector<vector<vector<double>>> trajGenerator();
+	/**
+	* generate trajectory profile with current time
+	*/
 	vector<vector<double>> trajGeneratorS();
+	/**
+	* calculate minimum time of trajectory of all DoF
+	*/
 	double minimumTime();
+	/**
+	* main function to compute time factors
+	* @param Jpeak maximum jerk
+	* @param Apeak maximum accerleration
+	* @param Vpeak maximum velocity
+	* @param a0 initial accelration
+	* @param v0 initial velocity
+	* @param p0 initial position
+	* @pram pG target position
+	* @param alpha time factor ratio
+	*/
 	vector<double> trajTimeT(double a0, double v0, double p0, double pG, double alpha, double mJ, double mA, double mV);
+	/**
+	* compute time factors of normal case
+	* @param Jpeak maximum jerk
+	* @param Apeak maximum accerleration
+	* @param Vpeak maximum velocity
+	* @param a0 initial accelration
+	* @param v0 initial velocity
+	* @param p0 initial position
+	* @pram pG target position
+	* @param alpha time factor ratio
+	*/
 	vector<double> trajTimeN(double a0, double v0, double p0, double pG, double alpha, double mJ, double mA, double mV);
+	/**
+	* compute time factors case but if the solution can not be found by normal case then output -1
+	* @param Jpeak maximum jerk
+	* @param Apeak maximum accerleration
+	* @param Vpeak maximum velocity
+	* @param a0 initial accelration
+	* @param v0 initial velocity
+	* @param p0 initial position
+	* @pram pG target position
+	* @param alpha time factor ratio
+	*/
 	vector<double> trajTimeT1(double a0, double v0, double p0, double pG, double alpha, double maxJ, double maxA, double maxV);
+	/**
+	* compute time factors with given accelration and velocity to target velocity
+	* @param Jpeak maximum jerk
+	* @param Apeak maximum accerleration
+	* @param Vpeak maximum velocity
+	* @param a0 initial accelration
+	* @param v0 initial velocity
+	* @param p0 initial position
+	* @pram pG target position
+	* @param alpha time factor ratio
+	*/
 	vector<double> trajTimeCall1(double a0, double v0, double p0, double vf, double alpha, double mJ, double mA, double mV,double s);
+	/**
+	* compute time factors to brake the initial acceleration and velocity to zero
+	* @param Jpeak maximum jerk
+	* @param Apeak maximum accerleration
+	* @param Vpeak maximum velocity
+	* @param a0 initial accelration
+	* @param v0 initial velocity
+	* @param p0 initial position
+	* @pram pG target position
+	* @param alpha time factor ratio
+	*/
 	vector<double> trajTimeCall2(double a0, double v0, double p0, double alpha, double mJ, double mA, double mV);
 	int num_dof_; // degree of freedom
 	double minT_; //minimal time
-	bool complete_;
+	bool complete_; // if trajecotry is complete
 	vector<double> alpha_; // time ratio
 	vector<double> mJ_; //max jerk
 	vector<double> mA_; //max acceleration
@@ -74,6 +422,6 @@ public:
 	vector<double> p0_; //initial position
 	vector<double> pG_; //target position
 	double rate_; // control loop rate (time interval)
-	vector<Times> trajTimes_;
-	int idx_;
+	vector<Times> trajTimes_; // store all time factors for each DoF
+	int idx_; // current profile index
 };
--- ROS/robot_controller/src/OTG/profileSegments.h
+++ ROS/robot_controller/src/OTG/profileSegments.h
@@ -12,23 +12,71 @@ using namespace std;
 class profileSeg
 {
 public:
-	double t1_,t2_,t11_,t22_,t33_;
-	double mJ_, mA_, mV_;
-	double alpha_;
-	double v0_, a0_;
-	double p0_;
-	double sa_;
-	vector<double> pre1_,pre2_,pre3_,pre4_,pre5_,pre6_,pre7_;
+	double t1_,t2_,t11_,t22_,t33_; //time factors
+	double mJ_, mA_, mV_; // maximum jerk, accelration, and velocity
+	double alpha_; // time factor ratio
+	double v0_, a0_;//initial velocity and accelration
+	double p0_;//initial position
+	double sa_;//trajectpry direction
+	vector<double> pre1_,pre2_,pre3_,pre4_,pre5_,pre6_,pre7_; //pre trajecotry segments
+	/**
+	* Constructor
+	* construct profileSeg object
+	* @param maxJ maximum jerk
+	* @param maxA maximum accerleration
+	* @param maxV maximum velocity
+	* @param a0 initial accelration
+	* @param v0 initial velocity
+	* @param p0 initial position
+	* @pram sa trajecotry direction
+	* @param alpha time factor ratio
+	* @param t11 t22 t33 t1 t2 time factors
+	*/
 	profileSeg(double v0, double a0, double p0, double alpha, double t11, double t22, double t33, double t1, double t2, double sa, double maxJ, double maxA, double maxV);
+	/**
+	* Destructor
+	*/
 	~profileSeg();
+	/**
+	* compute profile of segment 1
+	* @param t current time
+	*/
 	vector<double> Seg1(double t);
+	/**
+	* compute profile of segment 2
+	* @param t current time
+	*/
 	vector<double> Seg2( double t);
+	/**
+	* compute profile of segment 3
+	* @param t current time
+	*/
 	vector<double> Seg3( double t);
+	/**
+	* compute profile of segment 4
+	* @param t current time
+	*/
 	vector<double> Seg4( double t);
+	/**
+	* compute profile of segment 5
+	* @param t current time
+	*/
 	vector<double> Seg5( double t);
+	/**
+	* compute profile of segment 6
+	* @param t current time
+	*/
 	vector<double> Seg6( double t);
+	/**
+	* compute profile of segment 7
+	* @param t current time
+	*/
 	vector<double> Seg7( double t);
+	/**
+	* compute profile of segment 8
+	* @param t current time
+	*/
 	vector<double> Seg8( double t);
 private:
-	double Jpeak_,Apeak_, Vpeak_;
+	double Jpeak_,Apeak_, Vpeak_;// peak jerk, accelration, and velocity
 };
